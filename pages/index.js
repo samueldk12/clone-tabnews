41e7ba4aa006d4395ff3a25d1cf5ecaed956b16a
Samuel Arao <samuel.arao@gmail.com>
2025-03-31 11:16:33 -0300

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
-function Home(){
-    return <h1>Antonio, papai te ama muito! Não há palavras que descrevem o seu sorriso!</h1>
+function Home() {
+  return (
+    <h1>
+      Antonio, papai te ama muito! Não há palavras que descrevem o seu sorriso!
+    </h1>
+  );
 }
 
 export default Home;
