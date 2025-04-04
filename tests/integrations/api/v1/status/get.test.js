47d7eccf15253aff29a634532abb09b8ef283661
Samuel Arao <samuel.arao@gmail.com>
2025-04-04 10:29:04 -0300

    adicionado arquivo jsconfig.json'

--- tests/integrations/api/v1/status/get.test.js
+++ tests/integrations/api/v1/status/get.test.js
@@ -1,5 +1,5 @@
 test("GET to /api/v1/status should return 200", async () => {
-  console.log(process.env.URL_SITE.concat('api/v1/status'))
-  const response = await fetch(process.env.URL_SITE.concat('api/v1/status'));
+  console.log('http://localhost:3000/api/v1/status')
+  const response = await fetch('http://localhost:3000/api/v1/status');
   expect(response.status).toBe(200);
 });
