a8476e4f0acdff5ebcd22d7b09d24173192446e4
Samuel Arao <samuel.arao@gmail.com>
2025-05-06 11:52:43 -0300

    feat: ajuste testes para migrations

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -2,13 +2,30 @@ import migrationRunner from 'node-pg-migrate'
 import { join } from 'node:path'
 
 export default async function status(req, res) {
-  const migrations = await migrationRunner({
+  const defaultMigrationsOptions = {
     databaseUrl: process.env.DATABASE_URL,
     dryRun: true,
     dir: join('infra','migrations'),
     direction: 'up',
     migrationsTable: 'pgmigrations'
-  })
-  res.status(200).json(migrations);
+  }
+
+  if(req.method === "GET"){
+    const pedingsMigrations = await migrationRunner(defaultMigrationsOptions)
+    return res.status(200).json(pedingsMigrations);
+  }else if(req.method === "POST"){
+    const migratedMigrations = await migrationRunner(
+      {
+        ...defaultMigrationsOptions,
+        dryRun: false
+      }
+    )
+    if (migratedMigrations.length > 0){
+      return res.status(201).json(migratedMigrations)
+    }
+    return res.status(200).json(migratedMigrations);
+  }
+
+  return res.status(405).end()
 }
 
