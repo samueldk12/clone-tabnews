a6ed220f0b8f45c6c892f542f54f398fe0b2d09f
Samuel Arao <samuel.arao@gmail.com>
2025-05-26 15:22:00 -0300

    ajustar script teste

--- tests/integrations/api/v1/migrations/post.test.js
+++ tests/integrations/api/v1/migrations/post.test.js
@@ -7,30 +7,35 @@ beforeAll(async ()=>{
 })
 
 
-test("GET to /api/v1/migrations should return 200",  () => {
-  describe("Anonymous user", () => {
-    describe("Running pending migrations", () => {
-      test("For the first time", async () => {
-        let response = await fetch("http://localhost:3000/api/v1/migrations", {
-          method: "POST",
-        });
-        expect(response.status).toBe(201);
-        const responseBody = await response.json();
-        expect(Array.isArray(responseBody)).toBe(true);
-        expect(responseBody.length).toBeGreaterThan(0);
-      });
-      test("For the second time", async () => {
-        let secondResponse = await fetch(
-          "http://localhost:3000/api/v1/migrations",
-          {
-            method: "POST",
-          },
-        );
-        expect(secondResponse.status).toBe(200);
-        const secondResponseBody = await secondResponse.json();
-        expect(Array.isArray(secondResponseBody)).toBe(true);
-        expect(secondResponseBody.length).toBe(0);
-      });
-    });
+test("GET to /api/v1/migrations should return 200", async () => {
+
+  const response1 = await fetch("http://localhost:3000/api/v1/migrations",{
+    method: 'POST'
   });
+  expect(response1.status).toBe(201);
+
+  const response1Body = await response1.json();
+
+  const result_exist_table_query = await database.query(
+    "SELECT EXISTS (\
+      SELECT FROM information_schema.tables\
+      WHERE  table_schema = 'public'\
+      AND    table_name   = 'pgmigrations'\
+    );")
+  const exist_table = result_exist_table_query.rows[0].exists
+
+  expect(exist_table).toBe(true)
+  expect(Array.isArray(response1Body)).toBe(true);
+  expect(response1Body.length).toBeGreaterThan(0);
+
+  const response2 = await fetch("http://localhost:3000/api/v1/migrations",{
+    method: 'POST'
+  });
+  expect(response2.status).toBe(200);
+
+  const response2Body = await response2.json();
+
+  expect(exist_table).toBe(true)
+  expect(Array.isArray(response2Body)).toBe(true);
+  expect(response2Body.length).toBe(0);
 });
