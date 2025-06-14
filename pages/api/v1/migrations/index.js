2f8875d8000f5a566085a8843b26d0a2f144e91f
Samuel Arao <samuel.arao@gmail.com>
2025-06-14 17:58:46 -0300

    feat: refatoracao de migrations

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,8 +1,6 @@
-import migrationRunner from "node-pg-migrate";
-import { resolve } from "node:path";
-import database from "infra/database.js";
 import { createRouter } from "next-connect";
 import controller from "infra/controller";
+import migrator from "models/migrator";
 
 const router = createRouter();
 
@@ -10,43 +8,16 @@ router.get(getHandler).post(postHandler);
 
 export default router.handler(controller.errorHandlers);
 
-async function getDefaultMigrationsOptions(dbClient, dryRun) {
-  return {
-    dbClient: dbClient,
-    dryRun: dryRun,
-    dir: resolve("infra", "migrations"),
-    direction: "up",
-    migrationsTable: "pgmigrations",
-  };
-}
-
 async function getHandler(_, response) {
-  let dbClient;
-  try {
-    dbClient = await database.getNewClient();
-    const pedingsMigrations = await migrationRunner(
-      await getDefaultMigrationsOptions(dbClient, true),
-    );
-    dbClient.end();
-    return response.status(200).json(pedingsMigrations);
-  } finally {
-    dbClient?.end();
-  }
+  const pedingsMigrations = await migrator.listPendingMigrations();
+  return response.status(200).json(pedingsMigrations);
 }
 
 async function postHandler(_, response) {
-  let dbClient;
-  try {
-    dbClient = await database.getNewClient();
-    const migratedMigrations = await migrationRunner(
-      await getDefaultMigrationsOptions(dbClient, false),
-    );
-    dbClient.end();
-    if (migratedMigrations.length > 0) {
-      return response.status(201).json(migratedMigrations);
-    }
-    return response.status(200).json(migratedMigrations);
-  } finally {
-    dbClient?.end();
+  const migratedMigrations = await migrator.runPedingMigrations();
+
+  if (migratedMigrations.length > 0) {
+    return response.status(201).json(migratedMigrations);
   }
+  return response.status(200).json(migratedMigrations);
 }
