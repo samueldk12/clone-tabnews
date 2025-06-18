5ad2fbf6237c62eeb058453ebeaede8dfeca7003
Samuel Arao <samuel.arao@gmail.com>
2025-06-18 15:39:19 -0300

    feat: adiciona modulo users

--- infra/migrations/1749950174742_cursodev-create-users.js
+++ infra/migrations/1749950174742_cursodev-create-users.js
@@ -9,28 +9,30 @@ exports.up = (pgm) => {
     username: {
       type: "varchar(30)",
       notNull: true,
-      unique: true
+      unique: true,
     },
     // The max length of  a email is 254
-    email:{
+    email: {
       type: "varchar(254)",
       notNull: true,
-      unique: true
+      unique: true,
     },
-    // The max length of a dbcrypt use to password is 72
-    password:{
-      type: "varchar(72)",
+    // The max length of a dbcrypt use to password is 72, and always generate 60 characters
+    password: {
+      type: "varchar(60)",
       notNull: true,
     },
-    created_at:{
+    created_at: {
       type: "timestamptz",
-      default: pgm.func("now()"),
+      notNull: true,
+      default: pgm.func("timezone('utc',now())"),
     },
-    updated_at:{
+    updated_at: {
       type: "timestamptz",
-      default: pgm.func("now()"),
+      notNull: true,
+      default: pgm.func("timezone('utc',now())"),
     },
-  })
+  });
 };
 
 exports.down = false;
